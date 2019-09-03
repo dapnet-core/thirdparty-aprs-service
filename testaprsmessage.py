172d02260cce7ba59f82db28c56f878fa5dbcb04
Ralf Wilke <dh3wr@darc.de>
2019-09-03 15:33:44 +0000

    added tést python script

---
+++ testaprsmessage.py
@@ -0,0 +1,12 @@
+import logging
+import aprslib
+#logging.basicConfig(level=logging.DEBUG) # level=10
+
+
+# a valid passcode for the callsign is required in order to send
+AIS = aprslib.IS(callsign="DB0SDA-13", passwd="17330", host="aprs.db0sda.ampr.org", port=14580)
+AIS.connect()
+# send a single status message
+AIS.sendall("DB0SDA-13>APRS,TCPIP*::DH3WR-5  :Test von DAPNET567")
+
+#AIS.consumer(lambda x: None, raw=True)
